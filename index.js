8a7d5ca2365c5c3270584d34a61aa7ac62e050ff
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2022-06-18 16:29:34 +0600

    external router

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 const { request } = require('express');
 const express = require('express');
 const app = express();
-// const userRouter = require('./router');
+const userRouter = require('./router');
 
 // middleware
 app.use(express.json());
 app.use(express.raw());
 app.use(express.urlencoded());
 
-// router middleware
-const router = express.Router();
-app.use(router);
-
-router.get('/router', (req, res) => {
-  res.send('This is router');
-});
+app.use('/user', userRouter);
 
 // raw file reading request
-app.post('/raw', (req, res) => {
-  const result = req;
-  console.log(req.body);
-  res.send(req.body);
-});
+// app.post('/raw', (req, res) => {
+//   const result = req;
+//   console.log(req.body);
+//   res.send(req.body);
+// });
 
 app.listen(5000, (err, res) => {
   console.log('Server is listening on port 5000');
--- router.js
+++ router.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const userRouter = express.Router();
 
-userRouter.get('/', (req, res) => {
-  console.log('router module');
+userRouter.get('/create', (req, res) => {
+  console.log('router module first');
 });
 
 module.exports = userRouter;
