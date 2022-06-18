ce8b712c72cb7452071ef62b46eee2df3a191c53
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2022-06-18 20:56:40 +0600

    app.all method

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const express = require('express');
-
-const handler = require('./handler');
-const userRouter = require('./router');
-
 const app = express();
-const admin = express();
-
-app.use('/admin', admin);
-
-admin.get('/dashboard', (req, res) => {
-  res.send('admin dashboard');
+app.set('name', 'Ruhul');
+app.all('/admin', (req, res, next) => {
+  res.send('all route');
+  next();
 });
-
-app.use('/', userRouter);
+app.get('/admin', (req, res) => {
+  console.log(req.app.get('name'));
+  console.log(app.enabled());
+});
 
 app.listen(5000, (err, res) => {
   console.log('Server is listening on port 5000');
