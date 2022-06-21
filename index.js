ad02d548fdd7c5471b38cd2dcaff781d6409f44a
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2022-06-21 17:27:40 +0600

    configurable middleware

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
 
-function myMiddleware(req, res, next) {
-  try {
-    console.log(a);
-  } catch (err) {
-    throw Error(err.message);
-  }
-  console.log(b);
-  console.log('middleware1');
-  next();
+function logger(options) {
+  return (req, res, next) => {
+    if (options) {
+      console.log(`${req.url} => ${Date.now()}`);
+      next();
+    } else {
+      next();
+    }
+  };
 }
-function myMiddleware2(req, res, next) {
-  console.log('middleware2');
-  next();
-}
-// app.use(myMiddleware);
-app.use([myMiddleware, myMiddleware2]);
+// app.use(logger);
 
-app.get('/', function (req, res, next) {
-  console.log('hello1');
-  next();
-});
-app.get('/', function (req, res, next) {
+const userRouter = express.Router();
+userRouter.use(logger({ log: true }));
+userRouter.get('/list', function (req, res, next) {
+  res.send('hello user');
   next();
 });
 
-app.use(errorHandler);
+const adminRouter = express.Router();
+adminRouter.get('/list', function (req, res) {
+  res.send('hello admin');
+});
 
-function errorHandler(err, req, res, next) {
-  res.status(500).send(err.message);
-}
+app.use('/user', userRouter);
+app.use('/admin', adminRouter);
 
 app.listen(5000, (err, res) => {
   console.log('Server is listening on port 5000');
